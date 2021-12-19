798ffdf39e06cfb0f33aa8a22c5fb83ddf1a095b
Lessie Quezada <Lessieq07@gmail.com>
2021-12-19 17:15:46 -0500

    Used fetch to call the movieAPI

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,18 +1,37 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import Movie from './Movie'
+import SearchBar from './SearchBar'
 
 function MovieList(){
+    const [movies, setMovies]=useState([])
+    const [searchValue, setSearchValue]=useState('')
     const getMovie = async() =>{
-        const url = "http://www.omdbapi.com/?i=tt3896198&apikey=9c2b5f5f"
+        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=9c2b5f5f`
 
         const response = await fetch(url);
         const responseJson = await response.json();
         console.log(responseJson);
+
+        if(responseJson.Search){
+            setMovies(responseJson.Search)
+        }
+        
     }
     useEffect(() =>{
-        getMovie()
-    }, []);
+        getMovie(searchValue)
+    }, [searchValue]);
+
+    
+
     return(
-        <div></div>
+        <div>
+        <div>
+
+        <h1>Movies</h1> 
+        <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} />
+        <Movie movies={movies}/>
+        </div>
+        </div>
     )
 }
 
