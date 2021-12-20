8f81235eb94d7309a37b13a7eac1a5bcbf61e352
Lessie Quezada <Lessieq07@gmail.com>
2021-12-20 17:20:50 -0500

    Fixed and styled the movie cards

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,27 +1,23 @@
-import React from 'react'
-import '../App.css'
+import React from "react";
+import "../App.css";
 
-const Movie = (props) =>{
-    return(
-        <div>
-        {props.movies.map((movie,index)=>
-        <div className='row'>
-        <div className='column'>
-            <div className='card'>
-                <img src={movie.Poster} alt="movie-poster" style={{width:"100%"}}></img>
-                <div className='container'>
-                <h5>{movie.Title}</h5>
-                <br></br>
-                <h6>{movie.Year}</h6>
-                </div>
-            </div>
-            </div>
-            </div>
-            
-        )}
+const Movie = (props) => {
+  return (
+    <div
+      className="movie-container"
+      style={{ display: "flex", flexWrap: "wrap" }}
+    >
+      {props.movies.map((movie, index) => (
+        <div className="movie-content">
+          <img src={movie.Poster} alt="movie-poster"></img>
+          <div className="movie-info" >
+            <h3>{movie.Title}</h3>
+            <span>{movie.Year}</span>
+          </div>
         </div>
-        
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default Movie;
