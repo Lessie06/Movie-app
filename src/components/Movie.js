354500faefb64dfa2efd65d5199c88ce4c7a8aff
Lessie Quezada <Lessieq07@gmail.com>
2021-12-19 20:05:04 -0500

    Styled the movie into cards

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,16 +1,27 @@
 import React from 'react'
+import '../App.css'
 
 const Movie = (props) =>{
     return(
         <div>
         {props.movies.map((movie,index)=>
-            <div>
-                <img src={movie.Poster} alt="movie-poster"></img>
+        <div className='row'>
+        <div className='column'>
+            <div className='card'>
+                <img src={movie.Poster} alt="movie-poster" style={{width:"100%"}}></img>
+                <div className='container'>
+                <h5>{movie.Title}</h5>
+                <br></br>
+                <h6>{movie.Year}</h6>
+                </div>
             </div>
+            </div>
+            </div>
+            
         )}
-
         </div>
+        
     )
 }
 
-export default Movie
+export default Movie;
